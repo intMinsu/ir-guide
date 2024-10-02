cf178d5a2bf6317b1ec4a0506a6b4909e0d1ffba
intMinsu <rcvminsu@gmail.com>
2024-10-03 01:33:54 +0900

    Added comment

--- code/02_jetson_inference/simple_mobilenet_rtsp.py
+++ code/02_jetson_inference/simple_mobilenet_rtsp.py
@@ -38,6 +38,7 @@ parser.add_argument("input", type=str, default="v4l2:///dev/video0", nargs='?', 
 parser.add_argument("output", type=str, default="rtsp://localhost:8554/mystream", nargs='?', help="URI of the output stream")
 
 # [3. Network]
+# for more information, refer to https://github.com/dusty-nv/jetson-inference/blob/master/docs/detectnet-console-2.md
 parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre-trained model to load (see below for options)")
 parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
 parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use") 
